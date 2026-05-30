ceae57e0cfa4d14776174f37a9dab97f72062adf
Evelina Asay <_e.asai@yandex.ru>
2026-05-30 21:14:05 +0300

    clean: empty main branch for PR

--- tests/test_simple.py
+++
@@ -1,7 +0,0 @@
-﻿from selenium import webdriver
-
-def test_open_browser():
-    driver = webdriver.Chrome()
-    driver.get("https://www.google.com")
-    assert "Google" in driver.title
-    driver.quit()
